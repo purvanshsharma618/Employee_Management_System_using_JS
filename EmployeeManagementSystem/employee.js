aca6c7a3cecebdad846fc69352f9b0c2199ee90e
Purvansh Sharma <purvanshsharma6182gmail.com>
2025-03-05 10:42:06 +0530

    [Purvansh Sharma] UC3: Write a function to get work hours... problem solved.

--- EmployeeManagementSystem/employee.js
+++ EmployeeManagementSystem/employee.js
@@ -1,23 +1,24 @@
 // UC 1
-const IS_ABSENT =0
+const IS_ABSENT = 0;
 
-let empCheck = Math.floor(Math.random()*10)%2;
-if(empCheck == IS_ABSENT){
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck === IS_ABSENT) {
     console.log("Employee is Absent..");
 } else {
-    console.log("Employee is Present..")
+    console.log("Employee is Present..");
 }
 
-//UC 2
-const IS_PART_TIME =1;
-const IS_FULL_TIME =2;
-const PART_TIME_HOURS =4;
-const FULL_TIME_HOURS =8;
-const WAGE_PER_HOUR =20;
+// UC 2
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
 
-let empHrs =0;
-empCheck = Math.floor(Math.random()*10)%3;
-switch(empCheck){
+let empHrs = 0;
+empCheck = Math.floor(Math.random() * 10) % 3;
+
+switch (empCheck) {
     case IS_PART_TIME:
         empHrs = PART_TIME_HOURS;
         break;
@@ -25,7 +26,25 @@ switch(empCheck){
         empHrs = FULL_TIME_HOURS;
         break;
     default:
-        empHrs =0;
+        empHrs = 0;
 }
-let empWage = empHrs* WAGE_PER_HOUR;
-console.log("Emp Wage:" + empWage);
+
+let empWage = empHrs * WAGE_PER_HOUR;  // Defined empWage before using it
+console.log("Emp Wage: " + empWage);
+
+// UC 3
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+
+empCheck = Math.floor(Math.random() * 10) % 3; // Reusing empCheck
+empHrs = getWorkingHours(empCheck);
+empWage = empHrs * WAGE_PER_HOUR;
+console.log("Emp Wage: " + empWage);
